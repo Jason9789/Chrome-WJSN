b69ce6e33e4dddbb88d90e4d2533b67e9e44cac6
Jason9789 <lifebook0809@gmail.com>
2022-07-05 15:40:52 +0900

    add: Background Image 변경

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,13 @@ import User from './User';
 // import Weather from './Weather';
 
 import img1 from '../assets/background/background_01.jpeg';
+import img2 from '../assets/background/background_02.jpeg';
+import img3 from '../assets/background/background_03.jpeg';
+import img4 from '../assets/background/background_04.jpeg';
+import img5 from '../assets/background/background_05.jpeg';
+import img6 from '../assets/background/background_06.jpeg';
 
-const backgroundArr = [img1];
+const backgroundArr = [img1, img2, img3, img4, img5, img6];
 const randomIndex = Math.floor(Math.random() * backgroundArr.length);
 const backgroundImg = backgroundArr[randomIndex];
 
